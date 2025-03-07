38085be482a6a2546198f22d62bd9091f80d50bf
ramezhamada <ramezhamada@outlook.com>
2025-03-07 17:14:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from fake_useragent import UserAgent
 
 connections_per_thread = 50000000000000000
 num_threads = 50000000000000000
-url = "https://my.gov.ir/"
+url = "https://www.meta.ai/"
 
 referers = [
     "https://farsnews.ir/"
